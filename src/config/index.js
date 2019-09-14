3cb3e689c8272900f0fba4832b8f730f468aa21d
RobertDruska <robert.druska@gmail.com>
2019-09-15 00:07:09 +0200

    proxy

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 const configuration = {
-    apiUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://rocky-headland-25890.herokuapp.com',
+    // apiUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://rocky-headland-25890.herokuapp.com',
+    apiUrl: 'https://rocky-headland-25890.herokuapp.com',
 }
 
 export default configuration;
