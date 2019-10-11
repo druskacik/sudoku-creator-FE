d9ff7570e61c10f912e25dd42df970371f62966a
RobertDruska <robert.druska@gmail.com>
2019-10-11 18:47:34 +0200

    build added to tracked files

---
+++ build/precache-manifest.ac42820ded616e84a8e45b676d0eb1fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c4367507e87d30e9fdecf8256cd430",
+    "url": "/index.html"
+  },
+  {
+    "revision": "934377a089b90f189317",
+    "url": "/static/css/main.3db90b71.chunk.css"
+  },
+  {
+    "revision": "4a70b000e92d4c847499",
+    "url": "/static/js/2.092fef90.chunk.js"
+  },
+  {
+    "revision": "934377a089b90f189317",
+    "url": "/static/js/main.1600cfc9.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
